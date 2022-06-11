0a408a11956f97da486c4b5b69ff005659bb454c
Strayko <strayko_lol@windowslive.com>
2022-06-11 09:52:03 +0200

    jwt authentication

--- BuberDinner.Application/Services/Authentication/AuthenticationService.cs
+++ BuberDinner.Application/Services/Authentication/AuthenticationService.cs
@@ -1,7 +1,34 @@
-﻿namespace BuberDinner.Application.Services.Authentication;
+﻿using BuberDinner.Application.Common.Interfaces.Authentication;
+
+namespace BuberDinner.Application.Services.Authentication;
 
 public class AuthenticationService : IAuthenticationService
 {
+    private readonly IJwtTokenGenerator _jwtTokenGenerator;
+
+    public AuthenticationService(IJwtTokenGenerator jwtTokenGenerator)
+    {
+        _jwtTokenGenerator = jwtTokenGenerator;
+    }
+    public AuthenticationResult Register(string firstName, string lastName, string email, string password)
+    {
+        // Check if user already exists
+        
+        // Create user (generate unique ID)
+        
+        //Create JWT token
+        Guid userId = Guid.NewGuid();
+        var token = _jwtTokenGenerator.GenerateToken(userId, firstName, lastName);
+        
+        return new AuthenticationResult
+        (
+            Guid.NewGuid(),
+            firstName,
+            lastName,
+            email,
+            token
+        );
+    }
     public AuthenticationResult Login(string email, string password)
     {
         return new AuthenticationResult
@@ -9,18 +36,6 @@ public class AuthenticationService : IAuthenticationService
             Guid.NewGuid(),
             "firstName",
             "lastName",
-            email,
-            "token"
-        );
-    }
-
-    public AuthenticationResult Register(string firstName, string lastName, string email, string password)
-    {
-        return new AuthenticationResult
-        (
-            Guid.NewGuid(),
-            firstName,
-            lastName,
             email,
             "token"
         );
